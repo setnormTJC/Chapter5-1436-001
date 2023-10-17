adfafa8a3a75f2744dc5110e074e67ea4b349db9
Work <Work@MSI>
2023-10-17 09:52:14 -0500

    Where we ended in the morning on Oct 17.

--- Chapter5-1436-001/Source.cpp
+++ Chapter5-1436-001/Source.cpp
@@ -4,30 +4,80 @@ using namespace std;
 
 int main()
 {
-    //LOOPS/repetition 
+    ////LOOPS/repetition 
 
-    const int TOTAL_NUMBER_OF_STUDENTS = 5;
+    //const int TOTAL_NUMBER_OF_STUDENTS = 5;
 
-    int currentStudentNumber = 1; //"loop counter variable" 
+    //int currentStudentNumber = 1; //"loop counter variable" 
 
-    int currentStudentGrade;  //declare 
+    //int currentStudentGrade;  //declare 
 
-    double sumOfAllGrades = 0;
+    //double sumOfAllGrades = 0;
 
-    while (currentStudentNumber <= TOTAL_NUMBER_OF_STUDENTS)
+    //while (currentStudentNumber <= TOTAL_NUMBER_OF_STUDENTS)
+    //{
+    //    cout << "Enter student number " << currentStudentNumber << " 's grade: " << endl;
+    //    cin >> currentStudentGrade;
+
+    //    sumOfAllGrades = sumOfAllGrades + currentStudentGrade;
+
+    //    currentStudentNumber++; //C++!
+
+    //}
+
+    //double average = sumOfAllGrades / 5;
+
+    //cout << "\n\nAverage: " << average << endl;
+    ////ITSE 1302 
+
+    //int totalTimesToRun = 10'000;
+    //int loopCount = 0;
+
+    //while (loopCount < totalTimesToRun)
+    //{
+    //    loopCount++; //this is a shorthand notation to add 1
+    //    // This shorthand increment is also the basis for C++ name
+    //    cout << "Loop count is: " << loopCount << endl;
+    //}
+
+    //int dayCount = 0;
+    //while (dayCount <= 30)
+    //{
+    //    cout << "Exercise - " << dayCount << " days have passed!" << endl; ;
+
+    //    //dayCount++; 
+    //    //dayCount = dayCount + 1; 
+    //    dayCount = dayCount + 2;
+    //    //sets reminder to exercise every 3 days for a month
+    //}
+
+
+    //double countdown = 5.0;
+    //while (countdown >= 0)
+    //{
+    //    cout << countdown << "...";
+
+    //    //DECREMENTATION (to decrease - usually by 1) 
+    //    countdown = countdown - 0.1; //equivalently: countdown = countdown - 1
+    //}
+
+    //cout << "Liftoff.";
+
+    //int i = 3;
+    //while (i <= 100)
+    //{
+    //    cout << i << endl;
+    //    i = i * 2;
+    //    //i = i * i; //another example with different results
+    //}
+    //cout << "After loop ends, value of i is : " << i;
+    ////HI-LO game!
+
+
+    while (true)
     {
-        cout << "Enter student number " << currentStudentNumber << " 's grade: " << endl;
-        cin >> currentStudentGrade;
-
-        sumOfAllGrades = sumOfAllGrades + currentStudentGrade;
-
-        currentStudentNumber++; //C++!
-
+        cout << "Doing something useful - forever!" << endl;
     }
 
-    double average = sumOfAllGrades / 5;
-
-    cout << "\n\nAverage: " << average << endl;
-    //ITSE 1302 
     return 0;
 }
