269fa9bf42e9e9f89d5a9fc82ff4ba90b9e3ac6e
Work <Work@MSI>
2023-10-19 10:59:23 -0500

    Where we ended on Oct 19 in the morning.

--- Chapter5-1436-001/Source.cpp
+++ Chapter5-1436-001/Source.cpp
@@ -74,10 +74,76 @@ int main()
     ////HI-LO game!
 
 
-    while (true)
-    {
-        cout << "Doing something useful - forever!" << endl;
-    }
+    //while (true)
+    //{
+    //    cout << "Doing something useful - forever!" << endl;
+    //}
 
-    return 0;
+    //int a = 32; 
+
+    //cout << a / 0 << endl; 
+    //while (a > 500)
+    //{
+    //    cout << "a is greater than 500" << endl; 
+    //    a++; 
+    //    //no update the value of `a` here 
+
+    //} //end while loop 
+
+    //cout << "We are no  longer in the while loop!" << endl; 
+    //cout << "Enter some input ( q to quit): " << endl; 
+    //string response; 
+    //cin >> response; 
+    //while (response != "q");
+    //{
+    //    cout << "Enter input again (q to quit): " << endl; 
+
+    //}
+
+    //cout << "Out of the loop now" << endl; 
+
+    srand(time(0)); //don't worry about the details here 
+
+    //int loopCount = 0; 
+    //while (loopCount < 100)
+    //{
+   int randomNumber = rand() % 100; 
+   cout << "Guess the number (between 0 and 99) " << endl;
+   
+   //cout << "Hint - it is: " << randomNumber << endl; 
+
+   int guessedValue; 
+   cin >> guessedValue; 
+   
+   //LOSS condition
+   int numberOfGuesses = 1; 
+
+   //log(2, 100)
+
+   while (guessedValue != randomNumber &&  numberOfGuesses < 7)
+   {
+       if (guessedValue > randomNumber)
+       {
+           cout << "Too high - guess again: " << endl; 
+           cin >> guessedValue; 
+           numberOfGuesses++; 
+       }
+
+       else if (guessedValue < randomNumber)
+       {
+           cout << "Too low - guess again: " << endl; //hi-lo
+           cin >> guessedValue; 
+
+           numberOfGuesses++;
+       }
+
+       else
+       {
+           cout << "Correct! " << endl; 
+       }
+   } //end while 
+
+
+   cout << "You won!" << endl; 
+   return 0;
 }
